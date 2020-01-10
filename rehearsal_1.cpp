1bea7e87458594f2c8c0953b341b549d4f6fa405
mook <keettamook_k@cmu.ac.th>
2020-01-10 14:47:53 +0700

    finish re 1

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
 
+int main(){
+    float GPA;
+    string name;
+    cout << "What is your name?: ";
+    cin >> name;
+    cout << "\n" <<"What is your GPA?: ";
+    cin >> GPA;
+    if (GPA >=3.50){
+        cout <<"\n"<< name <<" LnW Jrim Jrim!!!";
+
+    }else{
+        cout <<"\n"<<"Try harder, "<< name <<"!!!";
+
+    }
+    return 0;
+
+
+
+
+}
