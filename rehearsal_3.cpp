25016b0ccdb943aa1d88b8597dd84a49fadd4508
mook <keettamook_k@cmu.ac.th>
2020-01-10 15:32:29 +0700

    finish r3

--- rehearsal_3.cpp
+++ rehearsal_3.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-
+#include<cmath>
+using namespace std;
 //Write sumSqrt() here.
-
+double sumSqrt(int N){
+    int i = 0,y = 0;
+    double z=0;
+    while(i<N){
+        z+=1/sqrt(i+1);
+        i++;
+        }
+    
+    if(N<=0){
+        return y;
+    }else{
+        return z;
+    }
+    
+    
+}
 
 int main()
 {
@@ -12,7 +28,8 @@ int main()
     double e = sumSqrt(1);
     double f = sumSqrt(0);
     double g = sumSqrt(-1);
-    
+ 
     cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n";
+
 
 }
